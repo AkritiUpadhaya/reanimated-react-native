4c22939d2cac6d4bf14042e67ffdd2769ea2bdc3
AkritiUpadhaya <upadhayaakriti23@gmail.com>
2025-05-30 20:33:17 +0545

    added animated component and shared value

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,13 +1,23 @@
-import Animated from 'react-native-reanimated';
+
+import { Button } from 'react-native';
+import Animated, { useSharedValue } from 'react-native-reanimated';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function HomeScreen() {
+  const width= useSharedValue(200)
+  const handlePress=()=>{
+    width.value= width.value+50;
+  }
   return (
+    <SafeAreaView>
     <Animated.View
       style={{
-        width: 100,
+        width,
         height: 100,
         backgroundColor: 'red',
       }}
     />
+    <Button title='animate' onPress={handlePress}/>
+    </SafeAreaView>
   );
 }
