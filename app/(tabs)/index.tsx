cd96e2e05f33ef0fcc4c307d514206e421d3e987
AkritiUpadhaya <upadhayaakriti23@gmail.com>
2025-06-04 18:53:51 +0545

    customizing animations

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,33 +1,58 @@
 import React from 'react';
-import { Button, StyleSheet, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 import Animated, {
-  useAnimatedProps,
+  Easing,
+  useAnimatedStyle,
   useSharedValue,
-  withTiming
+  withRepeat,
+  withTiming,
 } from 'react-native-reanimated';
 
+const duration = 2000;
+const width = useSharedValue(100);
+
+
+
 export default function App() {
-  const translateX = useSharedValue<number>(0);
+  const defaultAnim = useSharedValue<number>(width.value / 2 - 160);
+  const linear = useSharedValue<number>(width.value / 2 - 160);
 
-  const handlePress = () => {
-    translateX.value += 50;
-  };
+  const animatedDefault = useAnimatedStyle(() => ({
+    transform: [{ translateX: defaultAnim.value }],
+  }));
+  const animatedChanged = useAnimatedStyle(() => ({
+    transform: [{ translateX: linear.value }],
+  }));
 
-  const animatedProps=useAnimatedProps(()=>({
-    translateX:withTiming(translateX.value)
-  }))
-
-  // const animatedStyles = useAnimatedStyle(() => ({
-  //   transform: [{ translateX: withSpring(translateX.value * 2) }],
-  // }));
+  React.useEffect(() => {
+    linear.value = withRepeat(
+      // highlight-next-line
+      withTiming(-linear.value, {
+        duration,
+        easing: Easing.linear,
+      }),
+      -1,
+      true
+    );
+    defaultAnim.value = withRepeat(
+      // highlight-next-line
+      withTiming(-defaultAnim.value, {
+        duration,
+      }),
+      -1,
+      true
+    );
+  }, []);
 
   return (
-    <>
-      <Animated.View animatedProps={animatedProps} style={[styles.box]} />
-      <View style={styles.container}>
-        <Button onPress={handlePress} title="Click me" />
-      </View>
-    </>
+    <View style={styles.container}>
+      <Animated.View style={[styles.box, animatedDefault]}>
+        <Text style={styles.text}>inout</Text>
+      </Animated.View>
+      <Animated.View style={[styles.box, animatedChanged]}>
+        <Text style={styles.text}>linear</Text>
+      </Animated.View>
+    </View>
   );
 }
 
@@ -36,12 +61,21 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+    height: '100%',
   },
   box: {
-    height: 120,
-    width: 120,
-    backgroundColor: '#b58df1',
+    height: 80,
+    width: 80,
+    margin: 20,
+    borderWidth: 1,
+    borderColor: '#b58df1',
     borderRadius: 20,
-    marginVertical: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    color: '#b58df1',
+    textTransform: 'uppercase',
+    fontWeight: 'bold',
   },
 });
