47399cab77cf056f900125923dfeb50ef6873410
AkritiUpadhaya <upadhayaakriti23@gmail.com>
2025-06-05 15:43:20 +0545

    animated modifiers

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,7 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import Animated, {
-  Easing,
-  useAnimatedStyle,
-  useSharedValue,
-  withRepeat,
-  withTiming,
+import { Text, View } from 'react-native';
+import {
+  useSharedValue
 } from 'react-native-reanimated';
 
 const duration = 2000;
@@ -14,68 +10,73 @@ const width = useSharedValue(100);
 
 
 export default function App() {
-  const defaultAnim = useSharedValue<number>(width.value / 2 - 160);
-  const linear = useSharedValue<number>(width.value / 2 - 160);
+//   const defaultAnim = useSharedValue<number>(width.value / 2 - 160);
+//   const linear = useSharedValue<number>(width.value / 2 - 160);
 
-  const animatedDefault = useAnimatedStyle(() => ({
-    transform: [{ translateX: defaultAnim.value }],
-  }));
-  const animatedChanged = useAnimatedStyle(() => ({
-    transform: [{ translateX: linear.value }],
-  }));
+//   const animatedDefault = useAnimatedStyle(() => ({
+//     transform: [{ translateX: defaultAnim.value }],
+//   }));
+//   const animatedChanged = useAnimatedStyle(() => ({
+//     transform: [{ translateX: linear.value }],
+//   }));
 
-  React.useEffect(() => {
-    linear.value = withRepeat(
-      // highlight-next-line
-      withTiming(-linear.value, {
-        duration,
-        easing: Easing.linear,
-      }),
-      -1,
-      true
-    );
-    defaultAnim.value = withRepeat(
-      // highlight-next-line
-      withTiming(-defaultAnim.value, {
-        duration,
-      }),
-      -1,
-      true
-    );
-  }, []);
+//   React.useEffect(() => {
+//     linear.value = withRepeat(
+//       // highlight-next-line
+//       withTiming(-linear.value, {
+//         duration,
+//         easing: Easing.linear,
+//       }),
+//       -1,
+//       true
+//     );
+//     defaultAnim.value = withRepeat(
+//       // highlight-next-line
+//       withTiming(-defaultAnim.value, {
+//         duration,
+//       }),
+//       -1,
+//       true
+//     );
+//   }, []);
 
   return (
-    <View style={styles.container}>
-      <Animated.View style={[styles.box, animatedDefault]}>
-        <Text style={styles.text}>inout</Text>
-      </Animated.View>
-      <Animated.View style={[styles.box, animatedChanged]}>
-        <Text style={styles.text}>linear</Text>
-      </Animated.View>
-    </View>
-  );
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    height: '100%',
-  },
-  box: {
-    height: 80,
-    width: 80,
-    margin: 20,
-    borderWidth: 1,
-    borderColor: '#b58df1',
-    borderRadius: 20,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text: {
-    color: '#b58df1',
-    textTransform: 'uppercase',
-    fontWeight: 'bold',
-  },
-});
+    <View>
+      <Text style={{fontSize: 50}}>hello</Text>
+    </View>
+//     <View style={styles.container}>
+//       <Animated.View style={[styles.box, animatedDefault]}>
+//         <Text style={styles.text}>inout</Text>
+//       </Animated.View>
+//       <Animated.View style={[styles.box, animatedChanged]}>
+//         <Text style={styles.text}>linear</Text>
+//       </Animated.View>
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//     height: '100%',
+//   },
+//   box: {
+//     height: 80,
+//     width: 80,
+//     margin: 20,
+//     borderWidth: 1,
+//     borderColor: '#b58df1',
+//     borderRadius: 20,
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+//   text: {
+//     color: '#b58df1',
+//     textTransform: 'uppercase',
+//     fontWeight: 'bold',
+//   },
+// }
+);}
